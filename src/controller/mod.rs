f7d42b05508f1cf8c66f1bfe4f68b40e34593109
mashqar2022 <99820062+mashqar2022@users.noreply.github.com>
2022-02-21 09:52:15 +0200

    Remove unused code

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -2,11 +2,9 @@ use super::AppState;
 use std::sync::Mutex;
 
 pub mod group_controller;
-pub mod index_controller;
 pub mod user_controller;
 
 pub use group_controller::init as init_group_controller;
-pub use index_controller::init as init_index_controller;
 pub use user_controller::init as init_user_controller;
 
 fn log_request(route: &'static str, connections: &Mutex<u32>) {
